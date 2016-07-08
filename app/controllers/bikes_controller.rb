3b5d01f736d0d8b276ee1c6fcab6763ee8576766
Pam Willenz <pam@positek.net>
2016-07-08 21:33:08 +0000

    Add create on bike controller, view#show, trying to style without bootstrap

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,9 +1,26 @@
 class BikesController < ApplicationController
 
   def index
+    @bikes = Bike.all
   end
 
   def new
     @bike = Bike.new
+  end
+
+  def create
+    Bike.create(bike_params)
+    redirect_to root_path
+  end
+
+  def show
+    @bike = Bike.find(params[:id])
+    
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:name, :description)
   end
 end
