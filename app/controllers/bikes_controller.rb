c874e0793336c9f19575d7035b41fb2cd8f9ef72
Pam Willenz <pam@positek.net>
2016-07-08 20:47:19 +0000

    Build form for bike, new on controller, view#new, add simple_form gem, add bike resources to routes

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -2,4 +2,8 @@ class BikesController < ApplicationController
 
   def index
   end
+
+  def new
+    @bike = Bike.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 QuizOne::Application.routes.draw do
   root 'bikes#index'
+
+  resources :bikes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
